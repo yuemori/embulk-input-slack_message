8cb298e93ccd77c45752abce4bb924ed3221a3c6
yuemori <yuemori@aiming-inc.com>
2016-02-12 03:06:30 +0900

    First Implement

--- lib/embulk/input/slack_message.rb
+++ lib/embulk/input/slack_message.rb
@@ -1,18 +1,19 @@
+require 'slack'
+
 module Embulk
   module Input
-
     class SlackMessage < InputPlugin
       Plugin.register_input("slack_message", self)
 
       def self.transaction(config, &control)
-        # configuration code:
         task = {
           'channels' => config.param('channels', :array),
+          'token' => config.param('token', :string, default: nil),
           'latest' => config.param('latest', :string, default: Time.now.to_s),
-          'oldest' => config.param('latest', :string, default: 0),
-          'inclusive' => config.param('inclusive', :long, default: 0),
-          'count' => config.param('count', :long, default: 100),
-          'unreads' => config.param('unreads', :long, default: 0)
+          'oldest' => config.param('oldest', :string, default: 0),
+          'inclusive' => config.param('inclusive', :string, default: 0),
+          'count' => config.param('count', :string, default: 100),
+          'unreads' => config.param('unreads', :string, default: 0)
         }
 
         resume(task, self.columns, 1, &control)
@@ -20,13 +21,12 @@ module Embulk
 
       def self.columns
         [
-          Column.new(0, "messages", :string)
-          # Column.new(0, "channel_id", :string),
-          # Column.new(1, "channel_name", :string),
-          # Column.new(2, "datetime", :timestamp),
-          # Column.new(3, "user_id", :string),
-          # Column.new(4, "user_name", :string),
-          # Column.new(5, "message", :string)
+          Column.new(0, "datetime", :timestamp),
+          Column.new(1, "channel_id", :string),
+          Column.new(2, "channel_name", :string),
+          Column.new(3, "user_id", :string),
+          Column.new(4, "user_name", :string),
+          Column.new(5, "message", :string)
         ]
       end
 
@@ -46,42 +46,29 @@ module Embulk
         token = task['token'] || ENV['SLACK_TOKEN']
         raise StandardError.new, 'slack token is not found' unless token
 
-        @options = {
-          latest: Time.parse(task["latest"]),
-          oldest: Time.parse(task["oldest"])
-          inclusive: task['inclusive'],
-          count: task['count'],
-          unreads: task['unreads']
-        }
+        Slack.configure do |config|
+          config.token = token
+          config.options = {
+            latest: Time.parse(task["latest"]).to_f,
+            oldest: Time.parse(task["oldest"]).to_f,
+            inclusive: task['inclusive'],
+            count: task['count'],
+            unreads: task['unreads']
+          }
+        end
       end
 
       def run
         @channels.each do |channel_name|
-          channel_id = channels_list.find { |channel| channel['name'] == channel_name }['id']
-          options = @options.merge(channel: channel_id)
-          client.channels_history(options)['messages']
+          Slack.messages(channel_name).each do |message|
+            page_builder.add message
+          end
         end
 
-        page_builder.add(["example-value", 1, 0.1])
-        page_builder.add(["example-value", 2, 0.2])
         page_builder.finish
 
         task_report = {}
         return task_report
-      end
-
-      private
-
-      def users_list
-        @users_list ||= client.users_list["members"]
-      end
-
-      def channels_list
-        @channels_list ||= client.channels_list["channels"]
-      end
-
-      def client
-        Slack::Client.new token: @token
       end
     end
   end
